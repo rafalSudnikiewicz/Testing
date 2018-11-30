6acdd4a640f3e89dde4172a29b473bedd0a0c347
Zuravvski <m.zurawski1994@gmail.com>
2018-11-30 18:42:23 +0100

    Created basic project structure with a test sketch

--- src/test/java/pl/sda/CalculatorTests.java
+++ src/test/java/pl/sda/CalculatorTests.java
@@ -1,45 +1,23 @@
 package pl.sda;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-import pl.sda.Calculator;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class CalculatorTests {
 
-    private final Calculator calculator;
-    private final int firstNumber;
-    private final int secondNumber;
-    private final int expectedSumResult;
-
-    public CalculatorTests(int firstNumber, int secondNumber, int expectedSumResult) {
-        calculator = new Calculator();
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-        this.expectedSumResult = expectedSumResult;
-    }
-
-    @Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {2, 3, 5}, {4, 5, 9}
-        });
-    }
+    private Calculator calculator;
 
     @Test
     public void addingTwoNumbersShouldReturnExpectedResult() {
+        // Given
+        calculator = new Calculator();
+
         // When
-        int result = calculator.add(firstNumber, secondNumber);
+        int result = calculator.add(2, 3);
 
         // Then
-        assertEquals(expectedSumResult, result);
+        assertEquals(5, result);
     }
 }
 
