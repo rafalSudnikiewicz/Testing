f6edb39b8619a3d4d4184ad48d8e58d09892b23c
Zuravvski <m.zurawski1994@gmail.com>
2019-02-10 08:53:10 +0100

    Removed test implementations

--- src/test/java/pl/sda/advanced/UserServiceTests.java
+++ src/test/java/pl/sda/advanced/UserServiceTests.java
@@ -8,18 +8,6 @@ import pl.sda.advanced.users.UserService;
 import static org.junit.Assert.assertEquals;
 
 public class UserServiceTests {
-    private UserInMemoryDatabase userInMemoryDatabase;
-    private UserService userService;
 
-    @Before
-    public void setUp() throws Exception {
-        userInMemoryDatabase = Mockito.mock(UserInMemoryDatabase.class);
-        userService = new UserService(userInMemoryDatabase);
-    }
-
-    @Test
-    public void assumptionTest() {
-        assertEquals(0, userInMemoryDatabase.getUsersCount());
-    }
 }
 
